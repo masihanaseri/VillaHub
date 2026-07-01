75e678417a456ef8d8bc8ab4f0f1987754b95084
masiha <masihanaseri20@gmail.com>
2026-07-01 21:07:18 +0330

    first commit

---
+++ config/urls.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from accounts.views import (
+    SetActiveTownshipView,
+    SignupView,
+    CreateInvitationView,
+    AcceptInviteView,
+)
+
+urlpatterns = [
+
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
+
+    path(
+        "dashboard/",
+        include("dashboard.urls"),
+    ),
+
+    path(
+        "api/",
+        include("api.urls"),
+    ),
+
+    path(
+        "api/reservations/",
+        include("reservations.urls"),
+    ),
+
+    # -----------------------------
+    # Accounts
+    # -----------------------------
+
+    path(
+        "auth/signup/",
+        SignupView.as_view(),
+        name="signup",
+    ),
+
+    path(
+        "auth/set-township/",
+        SetActiveTownshipView.as_view(),
+        name="set-township",
+    ),
+
+    path(
+        "auth/invite/create/",
+        CreateInvitationView.as_view(),
+        name="create-invitation",
+    ),
+
+    path(
+        "auth/invite/<uuid:token>/accept/",
+        AcceptInviteView.as_view(),
+        name="accept-invite",
+    ),
+
+]
+
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
